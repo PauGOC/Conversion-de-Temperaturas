a2d32f0c92dc9b920fb580ad5178cedc0e9d0f35
PauGOC <141955969+PauGOC@users.noreply.github.com>
2023-08-20 20:09:37 -0600

    Add files via upload

---
+++ Conversion de Temperaturas.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include <locale.h>
+
+int main(){
+	int celsius=30;
+	int fahrenheit=5;
+	setlocale(LC_ALL,"");
+	system("cls");
+	printf("\033[1;31m" "Conversión de temperaturas \n\n");
+	printf("\033[1;34m" "Temperatura en Fahrenheit:"); 
+	printf("\033[0;37m %d °F \n", fahrenheit); //ANTES 
+	
+	fahrenheit=((fahrenheit-32)*5/9);
+	printf("\033[0;36mConversión de temperatura a Celcius:");
+	printf("\033[1;37m%d °C\n", fahrenheit);//Conversión a °C (1)
+	
+	printf("\n");
+	printf("-------------------------------------------------------\n\n");
+	
+	printf("\033[1;33mTemperatura en Celcius:");
+	printf("\033[1;37m%d °C\n", celsius); //ANTES 
+	
+	celsius=((celsius*9/5)+32);
+	printf("\033[0;33mConversión de temperatura a Fahrenheit:");
+	printf("\033[1;37m %d °F \n", celsius);//Conversión a °F
+	return 0;
+}
