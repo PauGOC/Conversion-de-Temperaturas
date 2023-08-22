9bace867380bc662a3bd04665af28cac68b774fb
PauGOC <141955969+PauGOC@users.noreply.github.com>
2023-08-22 11:59:15 -0600

    Update Conversion de Temperaturas.c

--- Conversion de Temperaturas.c
+++ Conversion de Temperaturas.c
@@ -2,26 +2,26 @@
 #include <locale.h>
 
 int main(){
-	int celsius=30;
-	int fahrenheit=5;
+	float celsius=30;
+	float fahrenheit=5;
 	setlocale(LC_ALL,"");
 	system("cls");
-	printf("\033[1;31m" "Conversión de temperaturas \n\n");
+	printf("\033[1;31m" "ConversiÃ³n de temperaturas \n\n");
 	printf("\033[1;34m" "Temperatura en Fahrenheit:"); 
-	printf("\033[0;37m %d °F \n", fahrenheit); //ANTES 
+	printf("\033[0;37m %f Â°F \n", fahrenheit); //ANTES 
 	
 	fahrenheit=((fahrenheit-32)*5/9);
-	printf("\033[0;36mConversión de temperatura a Celcius:");
-	printf("\033[1;37m%d °C\n", fahrenheit);//Conversión a °C (1)
+	printf("\033[0;36mConversiÃ³n de temperatura a Celcius:");
+	printf("\033[1;37m%f Â°C\n", fahrenheit);//ConversiÃ³n a Â°C (1)
 	
 	printf("\n");
 	printf("-------------------------------------------------------\n\n");
 	
 	printf("\033[1;33mTemperatura en Celcius:");
-	printf("\033[1;37m%d °C\n", celsius); //ANTES 
+	printf("\033[1;37m%f Â°C\n", celsius); //ANTES 
 	
 	celsius=((celsius*9/5)+32);
-	printf("\033[0;33mConversión de temperatura a Fahrenheit:");
-	printf("\033[1;37m %d °F \n", celsius);//Conversión a °F
+	printf("\033[0;33mConversiÃ³n de temperatura a Fahrenheit:");
+	printf("\033[1;37m %f Â°F \n", celsius);//ConversiÃ³n a Â°F
 	return 0;
 }
